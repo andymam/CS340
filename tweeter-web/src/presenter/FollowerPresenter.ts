63b05bcec667d6b1fb4f2465c04dbfde22d8150d
Andy Mam <118705791+andymam@users.noreply.github.com>
2025-11-02 09:40:39 -0700

    create tweeter-server module

--- tweeter-web/src/presenter/FollowerPresenter.ts
+++ tweeter-web/src/presenter/FollowerPresenter.ts
@@ -1,6 +1,6 @@
 import { AuthToken, User } from "tweeter-shared";
 import { UserItemPresenter } from "./UserItemPresenter";
-import { PAGE_SIZE, PagedItemView } from "./PagedItemPresenter";
+import { PAGE_SIZE } from "./PagedItemPresenter";
 
 export class FollowerPresenter extends UserItemPresenter {
   protected itemDescription(): string {
